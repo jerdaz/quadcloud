f11cec5552b454d987c89a642e00de47c3d90c5e
jerdaz <43273220+jerdaz@users.noreply.github.com>
2025-08-18 16:16:52 +0200

    Add keyboard shortcuts for quitting and quadrant focus

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, BrowserView, session, screen } = require('electron');
+const { app, BrowserWindow, BrowserView, session, screen, globalShortcut } = require('electron');
 const path = require('path');
 
 const URLs = [
@@ -22,6 +22,8 @@ function createView(x, y, width, height, index) {
   return view;
 }
 
+const views = [];
+
 function createWindow() {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
   const win = new BrowserWindow({ fullscreen: true, frame: false, autoHideMenuBar: true });
@@ -36,7 +38,27 @@ function createWindow() {
   positions.forEach((pos, i) => {
     const view = createView(pos.x, pos.y, viewWidth, viewHeight, i);
     win.addBrowserView(view);
+    views[i] = view;
+  });
+  views[0].webContents.focus();
+}
+
+function registerShortcuts() {
+  globalShortcut.register('CommandOrControl+Q', () => {
+    app.quit();
+  });
+  views.forEach((view, i) => {
+    globalShortcut.register(`CommandOrControl+${i + 1}`, () => {
+      view.webContents.focus();
+    });
   });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  registerShortcuts();
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
