2de18e6ac6ec29cd02d14694b6fa698f65c482a4
jerdaz <43273220+jerdaz@users.noreply.github.com>
2025-08-18 16:24:50 +0200

    Fix controller index parsing

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
-const params = new URLSearchParams(global.process.argv.slice(1).join('&'));
-const myIndex = Number(params.get('controllerIndex') || 0);
+const arg = global.process.argv.find(a => a.startsWith('--controllerIndex='));
+const myIndex = arg ? Number(arg.split('=')[1]) : 0;
 
 const nativeGetGamepads = navigator.getGamepads.bind(navigator);
 
