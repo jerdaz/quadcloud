cf2dd6b2244b454a9af974f4cdc8f7b8ee1c6030
jerdaz <43273220+jerdaz@users.noreply.github.com>
2025-08-19 19:08:57 +0200

    fix: rename windows launcher

--- quadcloud.bat
+++ quadcloud.bat
@@ -10,15 +10,15 @@ echo Updating source...
 if exist "%TMP_DIR%" rd /s /q "%TMP_DIR%"
 mkdir "%TMP_DIR%"
 curl -L %REPO_URL%/archive/refs/heads/%BRANCH%.zip -o "%TMP_DIR%\update.zip"
-tar -xf "%TMP_DIR%\update.zip" -C "%TMP_DIR%"
+powershell -NoLogo -NoProfile -Command "Expand-Archive -Path '%TMP_DIR%\update.zip' -DestinationPath '%TMP_DIR%'"
 xcopy "%TMP_DIR%\quadcloud-%BRANCH%\*" . /E /Y >nul
 rd /s /q "%TMP_DIR%"
 
 echo Installing latest Electron...
-npm install electron@latest --save >nul
+call npm install electron@latest --save >nul
 
 echo Installing dependencies...
-npm install >nul
+call npm install >nul
 
 echo Starting app...
-npm start
+call npm start
